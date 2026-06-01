0a978c5440cdea3d2ceab76830510117f798e4ac
Sara Caballero <saracaballerofr@hotmail.com>
2026-06-01 09:55:52 +0200

    Updates

--- assets/PrivacyPage-CYzmSFOr.js
+++ assets/PrivacyPage-CYzmSFOr.js
@@ -1,4 +1,4 @@
-import{c as u,r as f,j as e,l as v,C as j,t as k}from"./index-CyVn0fzz.js";import{A as N}from"./arrow-left-CVs0kVh4.js";/**
+import{c as u,r as f,j as e,l as v,C as j,t as k}from"./index-CIhr0FhS.js";import{A as N}from"./arrow-left-XivHPst5.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/arrow-left-XivHPst5.js
+++ assets/arrow-left-XivHPst5.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CyVn0fzz.js";/**
+import{c as e}from"./index-CIhr0FhS.js";/**
  * @license lucide-react v0.344.0 - ISC
  *
  * This source code is licensed under the ISC license.
